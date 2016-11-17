d84e4b71536ef52520f43dc44d6235669d27ea9a
raysibu <siburay5164@gmail.com>
2016-11-17 23:55:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var express = require('express');
+/**
+ express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
@@ -22,3 +23,25 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+**/
+
+
+
+(function($){
+  var googleUrl = "http://www.google.com?";
+
+ function fetchAndInsertResults(inputText){
+   $.ajax({ 
+     url: googleUrl+inputText, 
+     success: function(data){
+               $('#result').html(data);
+      }
+    });
+ }
+
+  $('#searchbutton').bind('submit',function(){
+    this.preventDefault();
+    var inputText = $('#searchForm').serialize();
+    fetchAndInsertResults(inputText);
+  });
+})(jQuery);
